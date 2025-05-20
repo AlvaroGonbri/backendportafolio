ea66aafd37e7af6c41d9f7494be8c8b6e28f4e1d
AlvaroGonbri <gonzalezbriones.alvaro@outlook.com>
2025-05-19 23:45:13 -0400

    Avance CRUD de usuarios

--- inventario/urls.py
+++ inventario/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import routers
 from accounts import views
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
