3103b15441355ba992e2435157fc9c4618e4b3c6
AlvaroGonbri <142188893+AlvaroGonbri@users.noreply.github.com>
2025-06-03 11:35:37 -0400

    Update settings.py

--- inventario/settings.py
+++ inventario/settings.py
@@ -60,7 +60,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'gonzalezbriones.alvaro@gmail.com'
-EMAIL_HOST_PASSWORD = 'iaphjswnuedkakup'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 ALERT_RECIPIENTS = ['gonzalezbriones.alvaro@gmail.com']
 
